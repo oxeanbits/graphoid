e03450f3cc44153cbccbf22a4c715eafd8ddc0c2
Victor Cordeiro Costa <victorcorcos@gmail.com>
2022-11-28 21:10:46 -0300

    Allow dynamicFields on sort by redfining a Hash ScalarType (still raise error: undefined method to_direction for {\"string_test\"=>\"ASC\"}:Hash\nDid you mean?  to_criteria)

--- lib/graphoid/definitions/sorter.rb
+++ lib/graphoid/definitions/sorter.rb
@@ -4,16 +4,18 @@ module Graphoid
   module Sorter
     LIST = {}
     @@enum_type = nil
+    @@dynamic_type = nil
 
     class << self
       def generate(model)
         LIST[model] ||= GraphQL::InputObjectType.define do
-          # binding.pry if model == Record
           name("#{Utils.graphqlize(model.name)}Sorter")
           description("Generated model Sorter for #{model.name}")
 
+          binding.pry if model == Record
           Attribute.fields_of(model).each do |field|
             name = Utils.camelize(field.name)
+            next argument(name, Sorter.dynamic_type) if name == 'dynamicFields'
             argument(name, Sorter.enum_type)
           end
 
@@ -37,6 +39,22 @@ module Graphoid
 
           value 'ASC', 'Ascendent'
           value 'DESC', 'Descendent'
+        end
+      end
+
+      def dynamic_type
+        @@dynamic_type ||= GraphQL::ScalarType.define do
+          name 'DynamicType'
+
+          def self.coerce_input(input_value, _context)
+            input_value.to_h
+          rescue StandardError
+            raise GraphQL::CoercionError, "#{input_value.inspect} is not a valid Hash"
+          end
+
+          def self.coerce_result(ruby_value, _context)
+            ruby_value.to_h
+          end
         end
       end
     end
