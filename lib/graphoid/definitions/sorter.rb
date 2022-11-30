d94268801956181fb9e099491e3ce1539aef4b42
Victor Cordeiro Costa <victorcorcos@gmail.com>
2022-11-30 18:39:41 -0300

    Debugger

--- lib/graphoid/definitions/sorter.rb
+++ lib/graphoid/definitions/sorter.rb
@@ -14,6 +14,7 @@ module Graphoid
 
           Attribute.fields_of(model).each do |field|
             name = Utils.camelize(field.name)
+            binding.pry if field.try(:opts).try(:dynamic)
             next argument(name, Sorter.dynamic_type) if field.try(:opts).try(:dynamic)
             argument(name, Sorter.enum_type)
           end
