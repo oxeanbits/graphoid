6ee2c4f272ece3d4f04def99779e220436c75d6a
Nilton Vasques <nilton.vasques@gmail.com>
2023-04-13 02:30:23 -0300

    rename project to digitalize

--- app/graphql/digitalize_schema.rb
+++ app/graphql/digitalize_schema.rb
@@ -1,4 +1,4 @@
-class GraphqlRailsSampleSchema < GraphQL::Schema
+class DigitalizeSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ end
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module GraphqlRailsSample
+module Digitalize
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
