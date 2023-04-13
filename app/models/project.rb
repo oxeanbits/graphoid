537e82e8cff092d0ac79795b5110ea7e8a84c20f
Nilton Vasques <nilton.vasques@gmail.com>
2023-04-13 02:22:51 -0300

    installing mongoid

---
+++ app/models/project.rb
@@ -0,0 +1,12 @@
+class Project < ApplicationRecord
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :id, type: String
+  field :name, type: String
+  field :active, type: Boolean, default: true
+
+  validates :name, presence: true
+  validates :id, presence: true
+  validates :active, presence: true
+end
