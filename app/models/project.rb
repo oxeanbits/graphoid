4aeb7b0c22584c53607737fecc40cac97c7e90c6
Nilton Vasques <nilton.vasques@gmail.com>
2023-04-16 23:47:29 -0300

    fixing field not declared

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,11 +3,12 @@ require 'graphoid/queries/queries'
 class Project < ApplicationRecord
   include Mongoid::Document
   include Mongoid::Timestamps
-  include Graphoid::Queries
 
-  field :id, type: String
+  #field :id, type: String # crashing due to duplicated field
   field :name, type: String
   field :active, type: Boolean, default: true
+
+  include Graphoid::Queries
 
   validates :name, presence: true
   validates :id, presence: true
