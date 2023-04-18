4f27302d1a0920edfda047fc600416c3b84e7734
Nilton Vasques <nilton.vasques@gmail.com>
2023-04-18 04:25:22 -0300

    add users with nested where tests

--- spec/requests/graphql_controller_spec.rb
+++ spec/requests/graphql_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe GraphqlController, type: :controller do
-  describe 'POST #execute' do
-    before do
-      User.delete_all
-      Project.delete_all
-    end
+  before do
+    User.delete_all
+    Project.delete_all
+  end
+
+  describe 'POST #execute projects' do
     let(:query) do
       <<-GRAPHQL
         {
@@ -76,6 +77,68 @@ RSpec.describe GraphqlController, type: :controller do
 
       it 'returns an error message' do
         expect(JSON.parse(response.body)['errors']).not_to be_empty
+      end
+    end
+  end
+
+  describe 'POST #execute users' do
+    let(:query) do
+      <<-GRAPHQL
+        {
+          users(where: { project: { nameContains: "BK" }}) {
+            name
+            email
+            active
+            projectId
+            projectIds
+            project { id name }
+          }
+        }
+      GRAPHQL
+    end
+
+    let!(:project1) { Project.create!(name: 'Project BK-A', active: true) }
+    let!(:project2) { Project.create(name: 'Project BK-B', active: true) }
+    let!(:project3) { Project.create!(name: 'Project C', active: true) }
+
+    let!(:user1) { User.create!(name: 'alice', email: 'alice@gmail.com',
+                                project_id: project1.id, project_ids: [project1.id.to_s],
+                                password: '12345678', password_confirmation: '12345678') }
+    let!(:user2) { User.create!(name: 'charlie', email: 'charlie@gmail.com',
+                                project_id: project2.id, project_ids: [project2.id.to_s],
+                                password: '12345678', password_confirmation: '12345678') }
+    let!(:user3) { User.create!(name: 'bob', email: 'bob@gmail.com',
+                                project_id: project3.id, project_ids: [project3.id.to_s],
+                                password: '12345678', password_confirmation: '12345678') }
+
+    context 'with valid query' do
+      before do
+        post :execute, params: { query: query }
+      end
+
+      it 'returns a 200 status code' do
+        expect(response).to have_http_status(200)
+      end
+
+      it 'returns the expected users' do
+        expect(JSON.parse(response.body)['data']['users']).to match_array([
+          {
+            'name' => user1.name,
+            'email' => user1.email,
+            'active' => user1.active,
+            'projectId' => project1.id.to_s,
+            'projectIds' => [project1.id.to_s],
+            'project' => { 'id' => project1.id.to_s, 'name' => project1.name }
+          },
+          {
+            'name' => user2.name,
+            'email' => user2.email,
+            'active' => user2.active,
+            'projectId' => project2.id.to_s,
+            'projectIds' => [project2.id.to_s],
+            'project' => { 'id' => project2.id.to_s, 'name' => project2.name }
+          }
+        ])
       end
     end
   end
