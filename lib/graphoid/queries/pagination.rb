3d9d6909ff71eb0afb70185393f04b808d0afdc7
Nilton Vasques <nilton.vasques@gmail.com>
2023-04-23 20:13:56 -0300

    pagination skip, limit, pages

--- lib/graphoid/queries/pagination.rb
+++ lib/graphoid/queries/pagination.rb
@@ -25,21 +25,9 @@ module Graphoid
         # https://www.rubydoc.info/github/rmosolgo/graphql-ruby/GraphQL/Field
         # Syntax => name, type, description
         field :count, GraphQL::Types::Int, null: true
-
-        field :pageSize, GraphQL::Types::Int, null: true
-
-        #field :pages, !types.Int, I18n.t('pagination.pages_hint') do
-        #  resolve lambda { |obj, _args, _ctx|
-        #    return 1 if obj.options[:limit].nil?
-
-        #    (obj.count / obj.options[:limit]) + 1
-        #  }
-        #end
-
-        #field :skip, !types.Int, I18n.t('pagination.skip_hint') do
-        #  resolve ->(obj, _args, _ctx) { obj.options[:skip] || 0 }
-        #end
-
+        field :page_size, GraphQL::Types::Int, null: true
+        field :pages, GraphQL::Types::Int, null: true
+        field :skip, GraphQL::Types::Int, null: true
         field :data, [grapho.type], null: true
 
         def data
@@ -52,52 +40,17 @@ module Graphoid
 
           object.options[:limit]
         end
+
+        def skip
+          object.options[:skip] || 0
+        end
+
+        def pages
+          return 1 if object.options[:limit].nil?
+
+          (object.count / object.options[:limit]) + 1
+        end
       end
-
-      #query_type.field name: grapho.plural, type: [grapho.type], null: true do
-      #  Graphoid::Argument.query_many(self, grapho.filter, grapho.order, required: false)
-      #end
-
-      #query_type.class_eval do
-      #  # Dynamically defining a resolver method for queries:
-      #  # query {
-      #  # project(id: "5e7b5b9b0b0b0b0b0b0b0b0b", where: { name_regex: "[a-z]" }) {
-      #  #  id
-      #  #  name
-      #  # }
-      #  define_method :"#{grapho.name}" do |id: nil, where: nil|
-      #    begin
-      #      return model.find(id) if id
-      #      Processor.execute(model, where.to_h).first
-      #    rescue Exception => ex
-      #      GraphQL::ExecutionError.new(ex.message)
-      #    end
-      #  end
-      #end
-
-      #query_type.class_eval do
-      #  # Dynamically defining a resolver method for queries:
-      #  # query {
-      #  #  projects(where: { name_contains: "a" }, order: { name: ASC }, limit: 10, skip: 10) {
-      #  #    id
-      #  #    name
-      #  # }
-      #  define_method :"#{grapho.plural}" do |where: nil, order: nil, limit: nil, skip: nil|
-      #    begin
-      #      # irep_node is deprecated
-      #      # maybe use context.ast_node instead
-      #      # but the problem is that it is not the same
-      #      # model = Graphoid.driver.eager_load(context.irep_node, model)
-      #      # https://graphql-ruby.org/fields/introduction.html#extra-field-metadata
-      #      result = Processor.execute(model, where.to_h)
-      #      order = Processor.parse_order(model, order.to_h)
-      #      result = result.order(order).limit(limit)
-      #      Graphoid.driver.skip(result, skip)
-      #    rescue Exception => ex
-      #      GraphQL::ExecutionError.new(ex.message)
-      #    end
-      #  end
-      #end
 
       query_type.field name: grapho.plural, type: paginated_type, null: true do
         argument :where, grapho.filter, required: false
