94ac41a2fa7f9a7b192507e4e010d7c455b06fb6
GiuseppeXD <giuseppelucam@hotmail.com>
2026-08-20 13:22:48 -0300

    OXEAN-133 Update relation selection spec

--- spec/tester_mongo/spec/graphoid/queries/attributes/subfilter_spec.rb
+++ spec/tester_mongo/spec/graphoid/queries/attributes/subfilter_spec.rb
@@ -25,87 +25,23 @@ describe 'QuerySubFilter', type: :request do
     [@h0, @a0, @p0, @l0, @l1].map(&:destroy)
   end
 
-  describe 'applies filters in related models' do
-    it 'when has many' do
+  describe 'when selecting related models' do
+    it 'should return all has-many relations after a scalar root filter' do
       @action = 'accounts'
       @query = %{
         query {
-          accounts {
+          accounts(where: { integerField: 2 }) {
             id
-            labels(where: { amount: 2, name: "l0" }) {
+            labels {
               id
             }
           }
         }
       }
 
-      expect(subject[0]['labels'].size).to eq(1)
-      expect(subject[0]['labels'][0]['id']).to eq @l0.id.to_s
+      expect(subject.size).to eq(1)
+      label_ids = subject[0]['labels'].map { |label| label['id'] }
+      expect(label_ids).to contain_exactly(@l0.id.to_s, @l1.id.to_s)
     end
-
-    # DEPRECATED: this is not supported anymore
-    # it 'when has one and does not match' do
-    #   @action = 'account'
-    #   @query = %{
-    #     query {
-    #       account {
-    #         id
-    #         person(where: { name: "something" }) {
-    #           id
-    #         }
-    #       }
-    #     }
-    #   }
-
-    #   expect(subject['person']).not_to be
-    # end
-
-    # it 'when has one and matches' do
-    #   @action = 'account'
-    #   @query = %{
-    #     query {
-    #       account {
-    #         id
-    #         person(where: { name: "p0" }) {
-    #           id
-    #         }
-    #       }
-    #     }
-    #   }
-
-    #   expect(subject['person']['id']).to eq @p0.id.to_s
-    # end
-
-    # it 'when belongs to and does not match' do
-    #   @action = 'account'
-    #   @query = %{
-    #     query {
-    #       account {
-    #         id
-    #         house(where: { name: "something" }) {
-    #           id
-    #         }
-    #       }
-    #     }
-    #   }
-
-    #   expect(subject['house']).not_to be
-    # end
-
-    # it 'when belongs to and does and matches' do
-    #   @action = 'account'
-    #   @query = %{
-    #     query {
-    #       account {
-    #         id
-    #         house(where: { name: "h0" }) {
-    #           id
-    #         }
-    #       }
-    #     }
-    #   }
-
-    #   expect(subject['house']['id']).to eq @h0.id.to_s
-    # end
   end
 end
