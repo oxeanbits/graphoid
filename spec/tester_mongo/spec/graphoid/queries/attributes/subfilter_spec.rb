4b3e4e785cf5b99549b222c8444637edadcb67b6
GiuseppeXD <giuseppelucam@hotmail.com>
2026-08-20 15:33:12 -0300

    OXEAN-133 Assert selected relation filter rejection

--- spec/tester_mongo/spec/graphoid/queries/attributes/subfilter_spec.rb
+++ spec/tester_mongo/spec/graphoid/queries/attributes/subfilter_spec.rb
@@ -1,47 +1,22 @@
 # frozen_string_literal: true
 
-# TODO: should apply submodel filtering on embeeded relations
-# TODO: elaborate more complex cases
-
 require 'rails_helper'
 
-describe 'QuerySubFilter', type: :request do
-  subject { Helper.resolve(self, @action, @query) }
-
-  before(:all) do
-    Person.delete_all
-    Label.delete_all
-    Account.delete_all
-    House.delete_all
-
-    @h0 = House.create!(name: 'h0')
-    @a0 = Account.create!(integer_field: 2, house: @h0)
-    @p0 = Person.create!(account: @a0, name: 'p0', snake_case: 'snake', camelCase: 'camel')
-    @l0 = Label.create!(account: @a0, name: 'l0', amount: 2)
-    @l1 = Label.create!(account: @a0, name: 'l1', amount: 2)
-  end
-
-  after(:all) do
-    [@h0, @a0, @p0, @l0, @l1].map(&:destroy)
-  end
-
-  describe 'when selecting related models' do
-    it 'should return all has-many relations after a scalar root filter' do
-      @action = 'accounts'
-      @query = %{
+describe 'QuerySubFilter' do
+  describe 'when filtering a selected relation' do
+    it 'should reject where on a has-many relation' do
+      errors = TesterMongoSchema.validate(<<~GRAPHQL)
         query {
-          accounts(where: { integerField: 2 }) {
+          accounts {
             id
-            labels {
+            labels(where: { amount: 2, name: "l0" }) {
               id
             }
           }
         }
-      }
+      GRAPHQL
 
-      expect(subject.size).to eq(1)
-      label_ids = subject[0]['labels'].map { |label| label['id'] }
-      expect(label_ids).to contain_exactly(@l0.id.to_s, @l1.id.to_s)
+      expect(errors.first.message).to include("Field 'labels' doesn't accept argument 'where'")
     end
   end
 end
